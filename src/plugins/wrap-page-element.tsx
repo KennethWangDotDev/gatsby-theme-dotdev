d4535e837fbdbf3ce14959be8d7c9ee84868f8ad
Kenneth Wang <kennethwang14@gmail.com>
2019-12-26 00:38:48 +0000

    improvement(wrap-page-element): added default background color to match subtle bg

--- src/plugins/wrap-page-element.tsx
+++ src/plugins/wrap-page-element.tsx
@@ -5,6 +5,7 @@ import SubtleBackground from '@assets/subtle/BgPage.png';
 
 const Background = styled.div`
     background: url(${SubtleBackground});
+    background-color: #e1d9cf;
     min-height: 100vh;
     padding-left: 1rem;
     padding-right: 1rem;
